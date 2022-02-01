9266fc13afaafd58a9f4a8abd8a4f907d8480f53
Michał Żak <michal.zak000@gmail.com>
2022-02-01 22:14:45 +0100

    A lot of new exceptions

--- src/ExceptionsComponent.py
+++ src/ExceptionsComponent.py
@@ -1,18 +1,169 @@
-# -------------------------------------------- LoggerComponent Exceptions ---------------------------------------------
+"""
+Exception with arguments template:
 
-class EncodeSetToFalse(Exception):
-    def __init__(self):
-        super().__init__(self)
+    class [EXCEPTION NAME](Exception):
+        def __init__(self, arg):
+            self.arg = str(arg)
+
+            super().__init__(self.arg)
+
+        def __str__(self):
+            return f" -> [MESSAGE]"
+
+Exception without arguments template:
+
+    class [EXCEPTION NAME](Exception):
+        def __init__(self):
+            super().__init__()
+
+        def __str__(self):
+            return f" -> [MESSAGE]"
+"""
+
+class FileNotFound(Exception):
+    def __init__(self, arg):
+        self.arg = str(arg)
+        super().__init__(self.arg)
 
     def __str__(self):
-        return f" -> Encode parameter has been set to False."
+        return f" -> The '{self.arg}' file has been not found."
 
-# ------------------------------------------- DatabaseComponent Exceptions --------------------------------------------
+class FileCorrupted(Exception):
+    def __init__(self, arg):
+        self.arg = str(arg)
+        super().__init__(self.arg)
+
+    def __str__(self):
+        return f" -> The '{self.arg}' file has been corrupted."
+
+class FileAlreadyExists(Exception):
+    def __init__(self, arg):
+        self.arg = str(arg)
+        super().__init__(self.arg)
+
+    def __str__(self):
+        return f" -> The '{self.arg}' file already exists."
+
+class FileCannotBeCreated(Exception):
+    def __init__(self, arg):
+        self.arg = str(arg)
+        super().__init__(self.arg)
+
+    def __str__(self):
+        return f" -> The '{self.arg}' file cannot be created."
+
+class FileCannotBeDeleted(Exception):
+    def __init__(self, arg):
+        self.arg = str(arg)
+        super().__init__(self.arg)
+
+    def __str__(self):
+        return f" -> The '{self.arg}' file cannot be deleted."
+
+
+class DirectoryNotFound(Exception):
+    def __init__(self, arg):
+        self.arg = str(arg)
+        super().__init__(self.arg)
+
+    def __str__(self):
+        return f" -> The '{self.arg}' directory has been not found."
+
+class DirectoryAlreadyExists(Exception):
+    def __init__(self, arg):
+        self.arg = str(arg)
+        super().__init__(self.arg)
+
+    def __str__(self):
+        return f" -> The '{self.arg}' directory already exists."
+
+class DirectoryCannotBeCreated(Exception):
+    def __init__(self, arg):
+        self.arg = str(arg)
+        super().__init__(self.arg)
+
+    def __str__(self):
+        return f" -> The '{self.arg}' directory cannot be created."
+
+class DirectoryCannotBeDeleted(Exception):
+    def __init__(self, arg):
+        self.arg = str(arg)
+        super().__init__(self.arg)
+
+    def __str__(self):
+        return f" -> The '{self.arg}' directory cannot be deleted."
+
+
+class ModuleCannotBeImported(Exception):
+    def __init__(self, arg):
+        self.arg = str(arg)
+        super().__init__(self.arg)
+
+    def __str__(self):
+        return f" -> The '{self.arg}' module cannot be imported."
+
+class ModuleCannotBeReloaded(Exception):
+    def __init__(self, arg):
+        self.arg = str(arg)
+        super().__init__(self.arg)
+
+    def __str__(self):
+        return f" -> The '{self.arg}' module cannot be reloaded."
+
+class ModuleCannotBeInstalled(Exception):
+    def __init__(self, arg):
+        self.arg = str(arg)
+        super().__init__(self.arg)
+
+    def __str__(self):
+        return f" -> The '{self.arg}' module cannot be installed."
+
+class ModuleCannotBeUninstalled(Exception):
+    def __init__(self, arg):
+        self.arg = str(arg)
+        super().__init__(self.arg)
+
+    def __str__(self):
+        return f" -> The '{self.arg}' module cannot be uninstalled."
+
+class ModuleIsIntegrated(Exception):
+    def __init__(self, arg):
+        self.arg = str(arg)
+        super().__init__(self.arg)
+
+    def __str__(self):
+        return f" -> The '{self.arg}' module is integrated."
+
+
+class InvalidModeException(Exception):
+    def __init__(self, arg):
+        self.arg = str(arg)
+        super().__init__(self.arg)
+
+    def __str__(self):
+        return f" -> The '{self.arg}' mode is invalid."
+
+class InvalidParameterException(Exception):
+    def __init__(self, arg):
+        self.arg = str(arg)
+        super().__init__(self.arg)
+
+    def __str__(self):
+        return f" -> The '{self.arg}' parameter is invalid."
+
+
+class LogCannotBeCreated(Exception):
+    def __init__(self, arg):
+        self.arg = str(arg)
+        super().__init__(self.arg)
+
+    def __str__(self):
+        return f" -> The '{self.arg}' log file cannot be created."
+
 
 class CannotOpenDatabase(Exception):
     def __init__(self, arg):
         self.arg = str(arg)
-
         super().__init__(self.arg)
 
     def __str__(self):
@@ -28,7 +179,6 @@ class CannotCreateCursor(Exception):
 class CannotCreateTable(Exception):
     def __init__(self, arg):
         self.arg = str(arg)
-
         super().__init__(self.arg)
 
     def __str__(self):
@@ -37,7 +187,6 @@ class CannotCreateTable(Exception):
 class TableAlreadyExists(Exception):
     def __init__(self, arg):
         self.arg = str(arg)
-
         super().__init__(self.arg)
 
     def __str__(self):
@@ -89,3 +238,10 @@ class CannotCloseConnection(Exception):
 
     def __str__(self):
         return " -> Cannot close the database."
+
+class EncodeSetToFalse(Exception):
+    def __init__(self):
+        super().__init__(self)
+
+    def __str__(self):
+        return f" -> Encode parameter has been set to False."
