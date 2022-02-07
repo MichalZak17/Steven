554f1203db2256989fceab9c02d0cd1251160f13
Michał Żak <michal.zak000@gmail.com>
2022-02-07 21:27:38 +0100

    The logging functions have been adjusted to the standard and the core thread controller has been fixed

--- src/GuardianComponent.py
+++ src/GuardianComponent.py
@@ -213,7 +213,7 @@ class GuardianClass:
         """
         try: 
             __caller_temp = inspect.getouterframes(inspect.currentframe(), 4)
-            __caller = f"{__caller_temp[1][1]}.{__caller_temp[1][3]}()"
+            __caller = f"src/GuardianComponent.py.{__caller_temp[1][3]}()"
         except: raise LogMessageCannotBeCreated()
 
         __levels = [
@@ -424,7 +424,7 @@ class GuardianClass:
         - Repairing: Thread is being repaired.
     """
 
-    def thread_start(self, start = False) -> None:
+    def thread_start(self) -> None:
         """
         Changes the status of the Guardian Core to online, if it's offline.
 
@@ -434,35 +434,27 @@ class GuardianClass:
         if not self.__THREAD_CURRENT_MODE == "online":
             self.__THREAD_CURRENT_MODE = "online"
             self.__create_log(arg = "Guardian Core mode has been changed to 'online'")
+        
+        try:
+            self.__THREAD = threading.Thread(target = self.__core)
+            self.__THREAD.start()
+        except:
+            self.__THREAD_CURRENT_MODE = "error"
+            self.__create_log(lvl = "error", err = "0x22", arg = "Guardian Core startup")
+            raise ThreadCannotBeStarted("Guardian Core")
 
-        if start:
-            try:
-                self.__THREAD = threading.Thread(target = self.__core)
-                self.__THREAD.start()
-            except:
-                self.__THREAD_CURRENT_MODE = "error"
-                self.__create_log(lvl = "error", err = "0x22", arg = "Guardian Core startup")
-                raise ThreadCannotBeStarted("Guardian Core")   
-            else: self.__create_log(arg = "Guardian Core has been started")
+        else: self.__create_log(arg = "Guardian Core has been started")
            
-    def thread_stop(self, kill = False) -> None:
+    def thread_stop(self) -> None:
         """
         Changes the status of the Guardian Core to offline, if it's online.
 
         Args:
             kill (bool): If True, the thread will be killed.
         """
-        if self.__THREAD_CURRENT_MODE == "online":
+        if not self.__THREAD_CURRENT_MODE == "online":
             self.__THREAD_CURRENT_MODE = "offline"            
             self.__create_log(arg = "Guardian Core has been turned 'offline'")
-
-        if kill:
-            self.__THREAD_CURRENT_MODE = "offline"
-            self.__create_log(arg = "Guardian Core has been turned 'offline'")
-
-            try: self.__THREAD.join()
-            except: pass
-            else: self.__create_log(arg = "Guardian Core has been killed")
 
     def thread_status(self, log = False) -> bool:
         """
@@ -483,107 +475,107 @@ class GuardianClass:
         while True:
             time.sleep(self.__THREAD_TIMEOUT)
 
-            if not self.__THREAD_CURRENT_MODE == "offline":
+            if not self.__THREAD_CURRENT_MODE == "offline": break
 
-                if not os.path.exists(self.__directory["data"]):
-                    for i in range(self.__TRIES):
-                        try: os.makedirs(self.__directory["data"])
-                        except: pass
-                        else: break
+            if not os.path.exists(self.__directory["data"]):
+                for i in range(self.__TRIES):
+                    try: os.makedirs(self.__directory["data"])
+                    except: pass
+                    else: break
 
-                    else: raise DirectoryCannotBeCreated(self.__directory["data"])
+                else: raise DirectoryCannotBeCreated(self.__directory["data"])
 
-                    for i in range(self.__TRIES):
-                        try: self.__log = open(self.__file["log"], "w")
-                        except: pass
-                        else:
-                            self.__log.close()
-                            break
+                for i in range(self.__TRIES):
+                    try: self.__log = open(self.__file["log"], "w")
+                    except: pass
+                    else:
+                        self.__log.close()
+                        break
 
-                    else: raise FileCannotBeCreated(self.__file["log"])
-                    
-                    self.__create_log(lvl = "warning", err = "0x5", arg = self.__directory["data"])
-                    self.__create_log(arg = f"The {self.__directory['data']} has been created")
-
-                    self.__create_log(lvl = "warning", err = "0x4", arg = self.__file["log"])
-                    self.__create_log(arg = f"The {self.__file['log']} has been created")
-
-                if not os.path.exists(self.__file["log"]):
-                    for i in range(self.__TRIES):
-                        try: self.__log = open(self.__file["log"], "w")
-                        except: pass
-                        else:
-                            self.__log.close()
-                            break
-
-                    else: raise FileCannotBeCreated(self.__file["log"])
-        
-                    self.__create_log(lvl = "warning", err = "0x4", arg = self.__file["log"])
-                    self.__create_log(arg = f"The {self.__file['log']} has been created")
-
-                for dir in self.__directory:
-                    self.__check_dir(dir = self.__directory[dir])
-
-                if self.__check_file(self.__file["dirstruct"]):
-                    try:
-                        with open(self.__file["dirstruct"], "rb") as f:
-                            self.__directory = pickle.load(f)
-                    except:
-                        self.__create_log(lvl = "warning", err = "0x16", arg = self.__file["dirstruct"])
-                        os.remove(self.__file["dirstruct"])
-                        self.__create_log(arg = f"The {self.__file['dirstruct']} has been removed")
+                else: raise FileCannotBeCreated(self.__file["log"])
                 
-                for e in os.listdir("."):
-                    if os.path.isfile(str(e)):
-                        if not str(e) in self.__file: self.__file[str(e)] = str(e)
-                    elif os.path.isdir(str(e)):
-                        if not str(e) in self.__directory: self.__directory[str(e)] = str(e)
+                self.__create_log(lvl = "warning", err = "0x5", arg = self.__directory["data"])
+                self.__create_log(arg = f"The {self.__directory['data']} has been created")
 
-                __temp_dict = self.__directory.copy()
-                        
-                for d in __temp_dict:
-                    if os.path.exists(d):
-                        for e in os.listdir(d):
+                self.__create_log(lvl = "warning", err = "0x4", arg = self.__file["log"])
+                self.__create_log(arg = f"The {self.__file['log']} has been created")
 
-                            if os.path.isfile(f"{d}/{e}"):
-                                if not f"{d}/{e}" in self.__file:
-                                    self.__file[f"{d}/{e}"] = f"{d}/{e}"
+            if not os.path.exists(self.__file["log"]):
+                for i in range(self.__TRIES):
+                    try: self.__log = open(self.__file["log"], "w")
+                    except: pass
+                    else:
+                        self.__log.close()
+                        break
 
-                            elif os.path.isdir(f"{d}/{e}"):                                
-                                if not f"{d}/{e}" in self.__directory:
-                                    self.__directory[f"{d}/{e}"] = f"{d}/{e}"
-                                                            
-                del __temp_dict
+                else: raise FileCannotBeCreated(self.__file["log"])
+    
+                self.__create_log(lvl = "warning", err = "0x4", arg = self.__file["log"])
+                self.__create_log(arg = f"The {self.__file['log']} has been created")
 
-                for dir in self.__directory:
-                    if not os.path.exists(self.__directory[dir]):
-                        self.__missing_directory.append(self.__directory[dir])
+            for dir in self.__directory:
+                self.__check_dir(dir = self.__directory[dir])
 
-                for file in self.__file:
-                    if not os.path.exists(self.__file[file]):
-                        self.__missing_file.append(self.__file[file])
+            if self.__check_file(self.__file["dirstruct"]):
+                try:
+                    with open(self.__file["dirstruct"], "rb") as f:
+                        self.__directory = pickle.load(f)
+                except:
+                    self.__create_log(lvl = "warning", err = "0x16", arg = self.__file["dirstruct"])
+                    os.remove(self.__file["dirstruct"])
+                    self.__create_log(arg = f"The {self.__file['dirstruct']} has been removed")
+            
+            for e in os.listdir("."):
+                if os.path.isfile(str(e)):
+                    if not str(e) in self.__file: self.__file[str(e)] = str(e)
+                elif os.path.isdir(str(e)):
+                    if not str(e) in self.__directory: self.__directory[str(e)] = str(e)
 
-                while len(self.__missing_directory) > 0:
-                    self.__create_log(lvl = "warning", err = "0x5", arg = self.__missing_directory[0])
+            __temp_dict = self.__directory.copy()
+                    
+            for d in __temp_dict:
+                if os.path.exists(d):
+                    for e in os.listdir(d):
 
-                    if not os.path.exists(self.__missing_directory[0]):
-                        for i in range(self.__TRIES):
-                            try: os.makedirs(self.__missing_directory[0])
-                            except: pass
-                            else:
-                                self.__create_log(arg = f"The {self.__missing_directory[0]} has been created")
-                                break
+                        if os.path.isfile(f"{d}/{e}"):
+                            if not f"{d}/{e}" in self.__file:
+                                self.__file[f"{d}/{e}"] = f"{d}/{e}"
 
-                        else: raise DirectoryCannotBeCreated(self.__missing_directory[0])
+                        elif os.path.isdir(f"{d}/{e}"):                                
+                            if not f"{d}/{e}" in self.__directory:
+                                self.__directory[f"{d}/{e}"] = f"{d}/{e}"
+                                                        
+            del __temp_dict
 
-                    self.__missing_directory.pop(0)
+            for dir in self.__directory:
+                if not os.path.exists(self.__directory[dir]):
+                    self.__missing_directory.append(self.__directory[dir])
 
-                while len(self.__missing_file) > 0:
-                    if not self.__missing_file[0] in self.__missing_file_flagged:
-                        self.__create_log(lvl = "warning", err = "0x4", arg = self.__missing_file[0])
-                        self.__missing_file_flagged.append(self.__missing_file[0])
-                        
-                    self.__missing_file.pop(0)
+            for file in self.__file:
+                if not os.path.exists(self.__file[file]):
+                    self.__missing_file.append(self.__file[file])
 
-                with open(self.__file["dirstruct"], "wb") as f:
-                    pickle.dump(self.__directory, f)
+            while len(self.__missing_directory) > 0:
+                self.__create_log(lvl = "warning", err = "0x5", arg = self.__missing_directory[0])
+
+                if not os.path.exists(self.__missing_directory[0]):
+                    for i in range(self.__TRIES):
+                        try: os.makedirs(self.__missing_directory[0])
+                        except: pass
+                        else:
+                            self.__create_log(arg = f"The {self.__missing_directory[0]} has been created")
+                            break
+
+                    else: raise DirectoryCannotBeCreated(self.__missing_directory[0])
+
+                self.__missing_directory.pop(0)
+
+            while len(self.__missing_file) > 0:
+                if not self.__missing_file[0] in self.__missing_file_flagged:
+                    self.__create_log(lvl = "warning", err = "0x4", arg = self.__missing_file[0])
+                    self.__missing_file_flagged.append(self.__missing_file[0])
+                    
+                self.__missing_file.pop(0)
+
+            with open(self.__file["dirstruct"], "wb") as f:
+                pickle.dump(self.__directory, f)
